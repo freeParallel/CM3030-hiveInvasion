9502f52ca26e8ee7db96a3a12e2666b8a1871306
freeParallel <84343291+freeParallel@users.noreply.github.com>
2025-07-12 20:46:50 +0200

    enemy approaches gate and atacks

--- CM3030-hiveInvasion/Assets/Scripts/EnemyMovement.cs
+++ CM3030-hiveInvasion/Assets/Scripts/EnemyMovement.cs
@@ -3,15 +3,54 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Transform target;
+    public float attackRange = 2f;
+    public int attackDamage = 10;
+    public float attackSpeed = 1f;
+    
     NavMeshAgent agent;
+    float lastAttackTime;
+    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         if (target != null)
         {
             agent.SetDestination(target.position);
+        }
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (distanceToTarget <= attackRange)
+        {
+            // stop when facing an obstacle
+            agent.isStopped = true;
+            AttackTarget();
+        }
+        else
+        {
+            // keep moving towards target
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void AttackTarget()
+    {
+        if (Time.time - lastAttackTime >= attackSpeed)
+        {
+            GateHealth gateHealth = target.GetComponent<GateHealth>();
+            if (gateHealth != null)
+            {
+                gateHealth.TakeDamage(attackDamage);
+                lastAttackTime =  Time.time;
+                Debug.Log("Enemy at the gates!");
+            }
         }
     }
 }
